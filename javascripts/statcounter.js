a6d06cc269810ca739c9e3c0916ef04b69cb7266
DR3WH0 <dr3wh0p5@gmail.com>
2013-08-31 09:04:34 -0500

    Update statcounter.js

--- javascripts/statcounter.js
+++ javascripts/statcounter.js
@@ -1,6 +1,6 @@
 //<![CDATA[
 var sc_project=9114352; 
-var sc_invisible=1; 
+var sc_invisible=0; 
 var sc_security="f55f8078"; 
 var scJsHost = (("https:" == document.location.protocol) ?
 "https://secure." : "http://www.");
