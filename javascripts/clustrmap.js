73cbfe52041e8499b11cb7818bcfea0eeb57bbf1
DR3WH0 <dr3wh0p5@gmail.com>
2013-09-03 04:29:30 -0500

    Delete clustrmap.js

--- javascripts/clustrmap.js
+++
@@ -1,9 +0,0 @@
-function cantload()
-{
-  img = document.getElementById("clustrMapsImg");
-  img.onerror = null;
-  img.src = "http://clustrmaps.com/images/clustrmaps-back-soon.jpg";
-  document.getElementById("clustrMapsLink").href = "http://clustrmaps.com";
-}
-img = document.getElementById("clustrMapsImg");
-img.onerror = cantload;
